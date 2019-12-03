270f6d8582b7e014ad75739abd39485205c76412
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-12-03 16:01:50 -0700

    Add style change for movieCard and login page

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -3,8 +3,11 @@ import './MovieCard.scss';
 
 const MovieCard = ({ title, episode_id, release_date }) => {
   return(
-    <article>
+    <article className='article-movie'>
       <h4>{title}</h4>
+      <h4>Episode: {episode_id}</h4>
+      <h4>Released: {release_date}</h4>
+      <button className='btn-characters'>View Characters</button>
     </article>
   )
 }
