dfb0eee3041f5ac1ea9e55b8439905fbf9236dcf
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-12-04 12:49:57 -0700

    Add testing package and testing config file

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import App from '../App/App.js'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
