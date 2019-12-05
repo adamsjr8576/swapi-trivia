94291b60a0e76d05f574014051f3ba700f0d9360
John Adams <adamsjr8576@gmail.com>
2019-12-04 20:36:25 -0700

    Add fetch calls for characters and combine data into single object

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './MovieCard.scss';
 
-const MovieCard = ({ title, episode_id, release_date }) => {
+const MovieCard = ({ title, episode_id, release_date, getCharacterData }) => {
+  console.log(typeof getCharacterData);
   return(
     <article className='article-movie'>
       <h4>{title}</h4>
       <h4>Episode: {episode_id}</h4>
       <h4>Released: {release_date}</h4>
-      <button className='btn-characters'>View Characters</button>
+      <button
+        id={episode_id}
+        className='btn-characters'
+        onClick={ (event) => getCharacterData(event.target.id) }>View Characters</button>
     </article>
   )
 }
--- src/Components/MovieContainer/MovieContainer.js
+++ src/Components/MovieContainer/MovieContainer.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './MovieContainer.scss';
 import MovieCard from '../MovieCard/MovieCard.js';
 
-const MovieContainer = ({ movieCards }) => {
+const MovieContainer = ({ movieCards, getCharacterData }) => {
   const movies = movieCards.sort((a, b) => a.episode_id - b.episode_id).map(card => {
     return(
-      <MovieCard 
+      <MovieCard
         title={card.title}
         episode_id={card.episode_id}
         release_date={card.release_date}
         key={card.episode_id}
+        getCharacterData={getCharacterData}
       />
     )
   })
