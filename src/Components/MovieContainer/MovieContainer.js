e0ffb2c8419517f29e7cc6e1650ed0d46f9e5802
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-12-08 19:41:14 -0700

    Add snapshot test to movie container

--- src/Components/MovieContainer/MovieContainer.js
+++ src/Components/MovieContainer/MovieContainer.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './MovieContainer.scss';
 import MovieCard from '../MovieCard/MovieCard.js';
 
-const MovieContainer = ({ movieCards, getCharacterData }) => {
+const MovieContainer = ({ movieCards }) => {
   const movies = movieCards.sort((a, b) => a.episode_id - b.episode_id).map(card => {
     return(
-      <MovieCard
+      <MovieCard 
         title={card.title}
         episode_id={card.episode_id}
         release_date={card.release_date}
         key={card.episode_id}
-        getCharacterData={getCharacterData}
       />
     )
   })
