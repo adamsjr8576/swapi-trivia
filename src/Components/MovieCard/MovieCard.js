41d6043dc6fc000b36b54f45e93619fc3c926cfc
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-12-09 22:32:07 -0700

    Add movie poster images and display them on cards

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -3,18 +3,21 @@ import './MovieCard.scss';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const MovieCard = ({ title, episode_id, release_date, getCharacterData }) => {
+const MovieCard = ({ title, episode_id, release_date, getCharacterData, poster }) => {
   return(
     <article className='article-movie'>
-      <h4>{title}</h4>
-      <h4>Episode: {episode_id}</h4>
-      <h4>Released: {release_date}</h4>
-      <Link to={`/movies/:${episode_id}`}>
-        <button
-          id={episode_id}
-          className='btn-characters'
-          onClick={ (event) => getCharacterData(event.target.id) }>View Characters</button>
-      </Link>
+      <h4 className='movie-card-title'>{title}</h4>
+      <div className='movie-card-div'>
+        <img src={poster} alt='movie poster' className='poster' />
+        <h4>Episode: {episode_id}</h4>
+        <h4>Released: {release_date}</h4>
+        <Link to={`/movies/:${episode_id}`}>
+          <button
+            id={episode_id}
+            className='btn-characters'
+            onClick={ (event) => getCharacterData(event.target.id) }>View Characters</button>
+        </Link>
+      </div>
     </article>
   )
 }
@@ -25,5 +28,6 @@ MovieCard.propTypes = {
   title: PropTypes.string,
   episode_id: PropTypes.number,
   release_date: PropTypes.string,
-  getCharacterData: PropTypes.func
+  getCharacterData: PropTypes.func,
+  poster: PropTypes.string
 }
--- src/images/images.js
+++ src/images/images.js
@@ -3,13 +3,31 @@ import errorGif from './giphy.gif';
 import falcon from './falcon.png';
 import rider from './rider.gif';
 import heart from './lover.png';
+import I from './Star-Wars-Episode-I-The-Phantom-Menace.jpg';
+import II from './Star-Wars-Episode-II-Attack-Of-The-Clones.jpg';
+import III from './Star-Wars-Episode-III-Revenge-Of-The-Sith.jpg';
+import IV from './Star-Wars-Episode-IV-A-New-Hope.jpg';
+import V from './Star-Wars-Episode-V-The-Empire-Strikes-Back.jpg';
+import VI from './Star-Wars-Episode-VI-Return-Of-The-Jedi.jpg';
+import VII from './Star-Wars-Episode-VII-The-Force-Awakens.jpg';
+
+
 
 const images = {
   starWarsLogo: starWarsLogo,
   errorGif: errorGif,
   falcon: falcon,
   rider: rider,
-  heart: heart
+  heart: heart,
+  posters: {
+    'The Phantom Menace': I , 
+    'Attack of the Clones': II , 
+    'Revenge of the Sith': III,
+    'A New Hope': IV,
+    'The Empire Strikes Back': V,
+    'Return of the Jedi': VI,
+    'The Force Awakens': VII
+  }
 }
 
 export default images
