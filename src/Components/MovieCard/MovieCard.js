eeaa31ca5bb5de94a30830ee4eda58ab7de3d410
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-12-09 23:24:33 -0700

    Add style to movie cards

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -9,14 +9,16 @@ const MovieCard = ({ title, episode_id, release_date, getCharacterData, poster }
       <h4 className='movie-card-title'>{title}</h4>
       <div className='movie-card-div'>
         <img src={poster} alt='movie poster' className='poster' />
-        <h4>Episode: {episode_id}</h4>
-        <h4>Released: {release_date}</h4>
-        <Link to={`/movies/:${episode_id}`}>
-          <button
-            id={episode_id}
-            className='btn-characters'
-            onClick={ (event) => getCharacterData(event.target.id) }>View Characters</button>
-        </Link>
+          <div className='info-div'>
+            <h4 className='h4-episode'>Episode: {episode_id}</h4>
+            <h4 className='h4-release'>Released: {release_date}</h4>
+            <Link to={`/movies/:${episode_id}`}>
+              <button
+                id={episode_id}
+                className='btn-characters'
+                onClick={ (event) => getCharacterData(event.target.id) }>View Characters</button>
+            </Link>
+          </div>
       </div>
     </article>
   )
