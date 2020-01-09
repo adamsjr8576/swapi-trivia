b44e4f088c5cc3a43fcbd5f6ebc6b35d1838dfae
John Adams <adamsjr8576@gmail.com>
2020-01-08 19:06:33 -0700

    Updates

---
+++ precache-manifest.f786c84ee8e431d55277dd0bbe6a2b13.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff5d3c7216a03e68a17d103efb439ad",
+    "url": "/swapi-trivia/index.html"
+  },
+  {
+    "revision": "ef9be2809d0efa5c463e",
+    "url": "/swapi-trivia/static/css/main.2acc37bf.chunk.css"
+  },
+  {
+    "revision": "cced03aec85dff34fdae",
+    "url": "/swapi-trivia/static/js/2.9b0b2138.chunk.js"
+  },
+  {
+    "revision": "ef9be2809d0efa5c463e",
+    "url": "/swapi-trivia/static/js/main.2a50ab4d.chunk.js"
+  },
+  {
+    "revision": "756b0db163c6124ad52e",
+    "url": "/swapi-trivia/static/js/runtime-main.39755281.js"
+  },
+  {
+    "revision": "1f1efd366b6be67b1e5e11e7324ecd1e",
+    "url": "/swapi-trivia/static/media/Star-Wars-Episode-I-The-Phantom-Menace.1f1efd36.jpg"
+  },
+  {
+    "revision": "b5b360385d719983f0e30c4b35ef97b2",
+    "url": "/swapi-trivia/static/media/Star-Wars-Episode-II-Attack-Of-The-Clones.b5b36038.jpg"
+  },
+  {
+    "revision": "662d154664154961ef9acc7b8c7efef1",
+    "url": "/swapi-trivia/static/media/Star-Wars-Episode-III-Revenge-Of-The-Sith.662d1546.jpg"
+  },
+  {
+    "revision": "853ec839cdd8acdca9bcd19d40f70890",
+    "url": "/swapi-trivia/static/media/Star-Wars-Episode-IV-A-New-Hope.853ec839.jpg"
+  },
+  {
+    "revision": "6376bec531aa1e42c67bac11d8980fe0",
+    "url": "/swapi-trivia/static/media/Star-Wars-Episode-V-The-Empire-Strikes-Back.6376bec5.jpg"
+  },
+  {
+    "revision": "6c7360618ef033aaeab136cfb7030e42",
+    "url": "/swapi-trivia/static/media/Star-Wars-Episode-VI-Return-Of-The-Jedi.6c736061.jpg"
+  },
+  {
+    "revision": "b70db613fdc9f5a65c59e56dd2e70e6d",
+    "url": "/swapi-trivia/static/media/Star-Wars-Episode-VII-The-Force-Awakens.b70db613.jpg"
+  },
+  {
+    "revision": "53b0292145359fc797768a5e043bef3f",
+    "url": "/swapi-trivia/static/media/errorGif.53b02921.gif"
+  },
+  {
+    "revision": "6dc534bb3306007218861d5972e28869",
+    "url": "/swapi-trivia/static/media/falcon.6dc534bb.png"
+  },
+  {
+    "revision": "8dd369624249fb239b2d4d97f02e868c",
+    "url": "/swapi-trivia/static/media/lover.8dd36962.png"
+  },
+  {
+    "revision": "5796f9fba4fa85e1916250702121187d",
+    "url": "/swapi-trivia/static/media/rider.5796f9fb.gif"
+  },
+  {
+    "revision": "e0118ff6862b6a2974d197ecd196ebab",
+    "url": "/swapi-trivia/static/media/starwarslogo.e0118ff6.png"
+  }
+]);
